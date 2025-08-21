1238ad46fd53fdff8eeadaa464cbd7a6aa06372d
Surya Prakash <surya0prakash@proton.me>
2025-08-21 23:03:22 +0530

    feat: command builder

---
+++ command/command.go
@@ -0,0 +1,30 @@
+package backend
+
+import "os/exec"
+
+type CommandBuilder struct{
+	name string
+	args []string
+}
+
+func New(name string) *CommandBuilder {
+	return &CommandBuilder{
+		name: name,
+		args: make([]string,0),
+	}
+}
+
+func (c *CommandBuilder) Arg(arg string) *CommandBuilder {
+	c.args = append(c.args, arg)
+	return c
+}
+
+
+func (c *CommandBuilder) Args(arg ...string) *CommandBuilder {
+	c.args = append(c.args, arg...)
+	return c
+}
+
+func (c *CommandBuilder) Build() *exec.Cmd {
+	return exec.Command(c.name,c.args...)
+}
---
+++ command/command_test.go
@@ -0,0 +1,15 @@
+package backend
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuilder(t *testing.T) {
+	builder := New("test").
+		Arg("-a").
+		Args("-b", "-c").
+		Build()
+	assert.Equal(t, []string{"test","-a", "-b", "-c"}, builder.Args)
+}
